96a40f9ffd838fe533426137a6fe15c239e3172b
Damien Kingsley <actuallydamo@gmail.com>
2022-01-26 20:09:59 +1100

    feat: install steam

--- setup.sh
+++ setup.sh
@@ -147,7 +147,8 @@ if [ $(hostname) = damo-desktop ]; then
   cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
   echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
   sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-  sudo apt update && sudo apt install signal-desktop
+  sudo apt update && sudo apt install -y signal-desktop \
+  steam
 fi
 
 if [ $(hostname) = damien-laptop ]; then
